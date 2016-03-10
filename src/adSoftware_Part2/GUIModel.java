ecd987f2c1b895f808f187d8831bbcd566a5231e
Oty99 <otonymanuel2@gmail.com>
2016-03-10 09:52:00 +0000

    Basic GUI and MVC set-up

---
+++ src/adSoftware_Part2/GUIModel.java
@@ -0,0 +1,10 @@
+package adSoftware_Part2;
+
+/**
+ * @author Otonye Manuel 
+ * holds the various tables information and orders in the kitchen 
+ * with get and set methods.Subject of observer pattern
+ */
+public class GUIModel {
+
+}
